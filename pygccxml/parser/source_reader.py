electronicvisions/pygccxml

commit 79d323fb42730a785283e027168c84231cdfd3c3

Norm file paths directly in source_reader
Instead of relying on a home-made method that adds complexity,
(and produces buggy results on windows), just call normpath directly.

pygccxml/parser/source_reader.py
@@ -466,17 +466,6 @@ class source_reader_t(object):
                 return file_path
         raise RuntimeError("pygccxml error: file '%s' does not exist" % file)
 
-    def __produce_full_file(self, file_path):
-        if os.name in ['nt', 'posix']:
-            file_path = file_path.replace(r'\/', os.path.sep)
-        if os.path.isabs(file_path):
-            return file_path
-        abs_file_path = os.path.realpath(
-            os.path.join(self.__config.working_directory, file_path))
-        if os.path.exists(abs_file_path):
-            return os.path.normpath(abs_file_path)
-        return file_path
-
     def __parse_xml_file(self, xml_file):
         scanner_ = scanner_t(xml_file, self.__decl_factory, self.__config)
         scanner_.read()
@@ -484,7 +473,7 @@ class source_reader_t(object):
         types = scanner_.types()
         files = {}
         for file_id, file_path in scanner_.files().items():
-            files[file_id] = self.__produce_full_file(file_path)
+            files[file_id] = os.path.normpath(file_path)
         linker_ = linker.linker_t(
             decls=decls,
             types=types,

commit 464825367daff2e364a5095220f173f23538a365

Fix bug using relative paths of parsed files
Bug was introduced in 79d323fb42730a785283e027168c84231cdfd3c3.
There, setting absolute paths was replaced by `os.path.normpath`,
which is not transforming to absolute paths.
As a result, objects with relative paths were not exposed.
Now, paths are patched with `os.path.abspath`.

Change-Id: Ic1fa23e11aeda05fd32b275563430339b85aa995

pygccxml/parser/source_reader.py
@@ -452,6 +452,7 @@ class source_reader_t(object):
         decls = scanner_.declarations()
         types = scanner_.types()
         files = scanner_.files()
+        files = {f_name: os.path.abspath(f) for f_name, f in files.items()}
         linker_ = linker.linker_t(
             decls=decls,
             types=types,